3e94e35146d8ebd13a7786afa43fd044ca64fb0a
Chirag Thakur <chirag11032000@gmail.com>
2019-10-20 10:34:00 +0530

    completed login/register

--- server/__init__.py
+++ server/__init__.py
@@ -5,7 +5,12 @@ from flask_migrate import Migrate
 
 app = Flask(__name__)
 app.config.from_object(Config)
+app.secret_key = "my secret key"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+@app.route("/")
+def home_page():
+    return "Hello there!"
 
 from server import models, auth
