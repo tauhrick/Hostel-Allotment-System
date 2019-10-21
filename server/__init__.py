36d5e45c5fcc7b039607bda2baeadc4eb6e421c5
rohit645 <kumarnath.rohit@gmail.com>
2019-10-22 00:57:38 +0530

    added one more floor

--- server/__init__.py
+++ server/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 from server.config import Config
 from flask_sqlalchemy import SQLAlchemy
-
 from flask_migrate import Migrate
+
 app = Flask(__name__)
 app.config.from_object(Config)
 app.secret_key = "my secret key"
