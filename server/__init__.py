4038589091f00be43b81c52228f0bb7e54943ae4
rohit645 <kumarnath.rohit@gmail.com>
2019-10-22 00:12:04 +0530

    added init file

--- server/__init__.py
+++ server/__init__.py
@@ -3,7 +3,6 @@ from server.config import Config
 from flask_sqlalchemy import SQLAlchemy
 
 from flask_migrate import Migrate
-
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
